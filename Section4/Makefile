7a678d25830d045ab49e9588de5df52a979efab7
SwitchTLL <74119752+SwitchTLL@users.noreply.github.com>
2022-04-20 20:03:01 +0300

    1
    last

---
+++ Section4/Makefile
@@ -0,0 +1,8 @@
+.PHONY: clean All
+
+All:
+	@echo "----------Building project:[ Section4Challenge - Debug ]----------"
+	@cd "Section4Challenge" && "$(MAKE)" -f  "Section4Challenge.mk"
+clean:
+	@echo "----------Cleaning project:[ Section4Challenge - Debug ]----------"
+	@cd "Section4Challenge" && "$(MAKE)" -f  "Section4Challenge.mk" clean
---
+++ Section4/Section5_Excs.1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  //Preprocessor directive
+
+int main()  // Function. Starting point of program execution.
+{
+    int favorite_number;  //Assign an integer an store it    
+    
+    std::cout << "Enter your favorite number between 1 and 100:";
+    std::cin >> favorite_number;  //Assign prestored integer by user input
+    
+    std::cout << "Amazing!! That's my favorite number too!" <<std::endl;
+    
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" <<std::endl;
+    
+    return 0;
+}
